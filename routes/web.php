6c0e8ff38af385f69671a1687c3019d0eb5c2648
carlosRobert2 <trabalho39@yahoo.com>
2024-10-05 20:59:43 -0300

    muitas mudanças e agora já com o sistema de Login e autenticação do usuário

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,24 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-#Route::get('/', function () {
-#   return view('welcome');
-#});
-
 Route::get('/', function () {
-    return view('tela_principal');
+    return view('welcome');
 });
 
+#Route::get('/welcome', function () {
+    #return view('tela_principal');
+#});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
